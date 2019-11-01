0eb983ebaa592ed1bc23d075cbcbfc77b2fd74f4
Tobi <bratchertobi@gmail.com>
2019-11-01 00:10:11 -0500

    tried to implement a show and hide feature with the different paragraphs that describe the sorts, failed horribly. Other than that changed the selection bars from div to clickable buttons so we can add some java script to them, fixed some more formatting issues, and considered changing majors

---
+++ pageeffects/homepage.js
@@ -0,0 +1,7 @@
+
+function displaySections(name) {
+var menuItems = document.getElementsByClassName(name)
+    menuItems[1].classList.add('hidden');
+    
+}
+/*grabs all the classes with the active uppper paragraph tag */
