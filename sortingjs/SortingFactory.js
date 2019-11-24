709921b816f6daa6ed558ad1e94ac88571f1e1c4
Tobi <bratchertobi@gmail.com>
2019-11-23 22:45:19 -0600

    updatedd sorts

--- sortingjs/SortingFactory.js
+++ sortingjs/SortingFactory.js
@@ -48,6 +48,7 @@ class Selection {
     constructor(list) {
         this.type = 'selection'
         this.list = list;
+        this.linkedList = new DoublyLinkedList();
     }
 
 
@@ -89,49 +90,48 @@ class Insertion {
         this.type = 'insertion';
         this.list = list;
         this.linkedList = new DoublyLinkedList();
-        //our linked list is a record from start to finish of
-        //what the sorting algorithim did
+        //our linked list is a record from start to finish of what the sorting algorithim did
       
     }
 
     //the sorting instance of the respective class, this generates everything from step 1 to step x
     sort() {
-        this.linkedList.add('start',this.list,1);
-        //save our starting point to the linked list
-        //save the list as the data we will pass to the animation generator
-        //save 1 as the jsline
 
-        if(this.list.length === 1){
+            this.linkedList.add('start',this.list,1);
+            //save the event, the data that involves this event, and the line of JS
+
+        if(this.list.length <= 1){
             return this.list;
-        }//check to see if the array is only 1 long
+        }//check to see if the array is only 1 long or lower
+            
+                this.linkedList.add('check',this.list.length,3);
+                //save the event, the data that involves this event, and the line of JS
 
         for (let comparator = 1; comparator < this.list.length ; comparator++) {
         
-        this.linkedList.add('index',comparator,3);
-        //save the first index to the linked list
-        //save the indice as the data we will pass to the animation generator
-        //save 3 as the jsline
+                this.linkedList.add('index',comparator,5);
+                //save the event, the data that involves this event, and the line of JS
           
            let comparee = comparator-1;
            //assign j equal to i
 
-        this.linkedList.add('index',comparee,5);
-        //save our smaller index to the linked list
-        //save the indice as the data we will pass to the animation generator
-        //save 3 as the jsline
+                this.linkedList.add('index',comparee,7);
+                //save the event, the data that involves this event, and the line of JS
 
            while(comparee>=0 && this.list[comparee] > this.list[comparator]){
-                //if our comparee is greater than our index and 
+                //if our comparee is bigger than the comparator
                 
                 this.linkedList.add('compare',[comparee,comparator],7);
-    
+                //save the event, the data that involves this event, and the line of JS
+
             let temp = this.list[comparee];
                 //save the larger element in a temp variable
             this.list[comparee] = this.list[comparator];
                 //overwrite the larger one with the smaller one, so bigger values float right
             this.list[comparator] = temp;
-                //FUNCTION save event splice (we move it in front of the element that is smaller)
+                
                 this.linkedList.add('swap',[comparator,comparee],9);
+                //save the event, the data that involves this event, and the line of JS
             comparee--;
             comparator--;
             //move both are comparee and comparator down one
@@ -185,28 +185,47 @@ class Bubble {
     constructor(list) {
         this.type = 'bubble';
         this.list = list;
+        this.linkedList = new DoublyLinkedList();
     }
 
 
     //the sorting instance of the respective class
     sort() {
+                        this.linkedList.add('start',this.list, 1);
+                        //save the event, the data for that event, and the line of javascript code
 
         if(this.list.length <= 1){
             return this.list;
         }//see if the list length is less than or equal to one if so just return it
-
+                        this.linkedList.add('check',this.list.length, 3);
+                        //save the event, the data for that event, and the line of javascript code
+        
         let arrayLength=this.list.length;
         //set the arrayLength equal to the length of the elements in the list
-
+                        this.linkedList.add('maxtorepeat',this.list.length, 5);
+                        //save the event, the data for that event, and the line of javascript code
         for(arrayLength;arrayLength>0;arrayLength--){
+
             for(let index=0; index <= this.list.length-1; index++){
-                if(this.list[index] > this.list[index+1]){
+
+                this.linkedList.add('comparator',index, 5);
+                //save the event, the data for that event, and the line of javascript code
+                this.linkedList.add('comparee',index, 5);
+                //save the event, the data for that event, and the line of javascript code
+
+                if(this.list[index] > this.list[index+1]){    
                     let temp = this.list[index];
                     this.list[index]= this.list[index+1];
                     this.list[index+1] = temp;
+
+                    this.linkedList.add('swap',[index,index+1], 7);
+                    //save the event, the data for that event, and the line of javascript code
+                
                 }//if statment compare the current element to the next one
             }//inner for loop selects the neighbors
         }//outter for loop repeats the algorithim for n times
+        this.linkedList.add('end',this.list, 10);
+                      
         return this.list;
     }
 
@@ -222,13 +241,18 @@ class Bubble {
 class Quick {
     constructor(list) {
         this.type = 'quick';
-        this.sortedList = list;
+        this.list = list;
+        this.linkedList = new DoublyLinkedList();
     }
 
 
     //the sorting instance of the respective class
     sort() {
 
+        if(this.list.length <= 1){
+            return this.list.length;
+        }//check to see if the list is = to or smaller than 1
+        //choose a pivot point here were using the median of three principle
     }
 
 
