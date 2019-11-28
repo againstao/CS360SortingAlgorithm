71960ad6b8c009af2568ca91d0f801dee64b169e
Tobi <bratchertobi@gmail.com>
2019-11-27 23:42:12 -0600

    got heap sort implemented and working

--- sortingjs/SortingFactory.js
+++ sortingjs/SortingFactory.js
@@ -1,9 +1,9 @@
-import {DoublyLinkedList} from "./linkedList.js";
+import { DoublyLinkedList } from "./linkedList.js";
 import { actionSwapIndices } from "./sortdisplay.js";
-export {SortingFactory,Insertion,Bubble,Selection,Merge,Quick,Heap}
+export { SortingFactory, Insertion, Bubble, Selection, Merge, Quick, Heap }
 class SortingFactory {
-    constructor(type,list){
-        this.create = sortingInstance(type,list);
+    constructor(type, list) {
+        this.create = sortingInstance(type, list);
         //returns an object of sort based off the type and list
         this.unsortedList = this.create.getList();
         //gets the unsorted list
@@ -12,9 +12,9 @@ class SortingFactory {
         this.recordOfSort = this.create.getLinkedList();
         //returns all the records that the linked list created 
     }
-    
+
     sortingInstance(type, list) {
-        switch(type){
+        switch (type) {
             case 'selection':
                 return new Selection(list)
                 break;
@@ -38,7 +38,7 @@ class SortingFactory {
     }//in a programming since a factory is just an object that creates
     //or manufactures different objects
 
-    
+
 
 
 }//sorting factory
@@ -56,32 +56,32 @@ class Selection {
 
     //the sorting instance of the respective class
     sort() {
-        
-        if(this.list.length <= 1){
+
+        if (this.list.length <= 1) {
             return this.list;
         }//check to see if the list is only one element long or shorter
-        
-       for(let indexStart = 0; indexStart <= this.list.length-1;indexStart++){
-       
-        let minIndex = null;
 
-        for(let indexIterator = indexStart; indexIterator <= this.list.length-1;indexIterator++){
-           if(this.list[indexIterator] < this.list[minIndex] || minIndex === null){
-            minIndex=indexIterator;//searching for lowest index
-           } 
+        for (let indexStart = 0; indexStart <= this.list.length - 1; indexStart++) {
+
+            let minIndex = null;
+
+            for (let indexIterator = indexStart; indexIterator <= this.list.length - 1; indexIterator++) {
+                if (this.list[indexIterator] < this.list[minIndex] || minIndex === null) {
+                    minIndex = indexIterator;//searching for lowest index
+                }
+            }
+            let smallerNumber = this.list[indexStart];
+            this.list[indexStart] = this.list[minIndex];
+            this.list[minIndex] = smallerNumber;
         }
-        let smallerNumber = this.list[indexStart];
-        this.list[indexStart] = this.list[minIndex];
-        this.list[minIndex] = smallerNumber;
-       }
         return this.list;
     }//sort
-    
-    getLinkedList(){
+
+    getLinkedList() {
         return this.linkedList;
     }
 
-    getList(){
+    getList() {
         return this.list;
     }
 
@@ -95,68 +95,68 @@ class Insertion {
         this.list = list;
         this.linkedList = new DoublyLinkedList();
         //our linked list is a record from start to finish of what the sorting algorithim did
-      
+
     }
 
     //the sorting instance of the respective class, this generates everything from step 1 to step x
     sort() {
 
-            this.linkedList.add('start',this.list,1);
-            //save the event, the data that involves this event, and the line of JS
+        this.linkedList.add('start', this.list, 1);
+        //save the event, the data that involves this event, and the line of JS
 
-        if(this.list.length <= 1){
-            this.linkedList.add('end',this.list.length,3);
-                //save the event, the data that involves this event, and the line of JS
+        if (this.list.length <= 1) {
+            this.linkedList.add('end', this.list.length, 3);
+            //save the event, the data that involves this event, and the line of JS
             return this.list;
         }//check to see if the array is only 1 long or lower
-            
-                this.linkedList.add('check',this.list.length,5);
-                //save the event, the data that involves this event, and the line of JS
 
-        for (let comparator = 1; comparator < this.list.length ; comparator++) {
-        
-                this.linkedList.add('index',comparator,7);
-                //save the event, the data that involves this event, and the line of JS
-          
-           let comparee = comparator-1;
-           //assign j equal to i
+        this.linkedList.add('check', this.list.length, 5);
+        //save the event, the data that involves this event, and the line of JS
 
-                this.linkedList.add('index',comparee,9);
-                //save the event, the data that involves this event, and the line of JS
+        for (let comparator = 1; comparator < this.list.length; comparator++) {
 
-           while(comparee>=0 && this.list[comparee] > this.list[comparator]){
+            this.linkedList.add('index', comparator, 7);
+            //save the event, the data that involves this event, and the line of JS
+
+            let comparee = comparator - 1;
+            //assign j equal to i
+
+            this.linkedList.add('index', comparee, 9);
+            //save the event, the data that involves this event, and the line of JS
+
+            while (comparee >= 0 && this.list[comparee] > this.list[comparator]) {
                 //if our comparee is bigger than the comparator
-                
-                this.linkedList.add('compare',[comparee,comparator],11);
+
+                this.linkedList.add('compare', [comparee, comparator], 11);
                 //save the event, the data that involves this event, and the line of JS
 
-            let temp = this.list[comparee];
+                let temp = this.list[comparee];
                 //save the larger element in a temp variable
-            this.list[comparee] = this.list[comparator];
+                this.list[comparee] = this.list[comparator];
                 //overwrite the larger one with the smaller one, so bigger values float right
-            this.list[comparator] = temp;
-                
-                this.linkedList.add('swap',[comparator,comparee],13);
-                //save the event, the data that involves this event, and the line of JS
-            comparee--;
-            comparator--;
-            //move both are comparee and comparator down one
+                this.list[comparator] = temp;
 
-                this.linkedList.add('index',comparee,15);
-                this.linkedList.add('index',comparator,16);
-           }//while
+                this.linkedList.add('swap', [comparator, comparee], 13);
+                //save the event, the data that involves this event, and the line of JS
+                comparee--;
+                comparator--;
+                //move both are comparee and comparator down one
+
+                this.linkedList.add('index', comparee, 15);
+                this.linkedList.add('index', comparator, 16);
+            }//while
 
         }//for
-        this.linkedList.add('end',this.list,19);
+        this.linkedList.add('end', this.list, 19);
         return this.list;
     }//sort 
     //hasnt been unit tested
 
-    getLinkedList(){
+    getLinkedList() {
         return this.linkedList;
     }
 
-    getList(){
+    getList() {
         return this.list;
     }
 
@@ -173,42 +173,40 @@ class Merge {
     mergeSort() {
         const sortedArray = [...list]; //new array with content of list.
         const length = sortedArray.length;
-        
+
         //Base Case
         //If there are 0 or 1 elements, stop splitting.
-        if (length < 2){
+        if (length < 2) {
             return list;
-        } 
-        
-        const mid = Math.floor(length/2); //Math.floor rounds down.
-        const left = sortedArray.slice(0,mid); // Left sub-array [0] to [mid-1].
+        }
+
+        const mid = Math.floor(length / 2); //Math.floor rounds down.
+        const left = sortedArray.slice(0, mid); // Left sub-array [0] to [mid-1].
         const right = sortedArray.slice(middle)// Right sub-array from index[middle] to [length-1];
-        
+
         //Recursive Step
-        return merge(mergeSort(left),mergeSort(right));
+        return merge(mergeSort(left), mergeSort(right));
     }
 
     merge(left, right) {
         const aux = []; //Auxillary array
         while (left.length && right.length) {
-            if (left[0] <= right[0])
-                {
-                    aux.push(left.shift());
-                }//If the first value in left sub-array, remove and add to auxillary array.
-            else 
-                {
-                    aux.push(right.shift());
-                }//else, remove and add first value in right sub-array to auxillary array.
-            return aux.concat(left,right);//Merge remaining items of left and right sub-arrays with the new array. 
-            
+            if (left[0] <= right[0]) {
+                aux.push(left.shift());
+            }//If the first value in left sub-array, remove and add to auxillary array.
+            else {
+                aux.push(right.shift());
+            }//else, remove and add first value in right sub-array to auxillary array.
+            return aux.concat(left, right);//Merge remaining items of left and right sub-arrays with the new array. 
+
         }
     }//Helper function
-    
-    getLinkedList(){
+
+    getLinkedList() {
         return this.linkedList;
     }
 
-    getSortedList(){
+    getSortedList() {
         return this.sortedList;
     }
 }
@@ -217,103 +215,114 @@ class Heap {
     constructor(list) {
         this.type = 'heap';
         this.list = list;
-        
+        this.linkedList = new DoublyLinkedList();
     }
 
-    buildMinHeap(){
+    buildMaxHeap() {
 
-        for(let indexIterator = 0; indexIterator <= this.list.length-1; indexIterator++){
+        for (let indexIterator = 0; indexIterator <= this.list.length - 1; indexIterator++) {
 
-            let leftChild = (indexIterator*2)+1;
-            let rightChild = (indexIterator*2)+2;
+            let leftChild = (indexIterator * 2) + 1;
+            let rightChild = (indexIterator * 2) + 2;
 
             //if the left child is smaller than the parent    
-            if(this.list[indexIterator] > this.list[leftChild] && leftChild<=this.list.length-1){
+            if (this.list[indexIterator] < this.list[leftChild] && leftChild <= this.list.length - 1) {
                 let temp = this.list[leftChild];
                 this.list[leftChild] = this.list[indexIterator];
                 this.list[indexIterator] = temp;
-    
-                indexIterator=0;
+
+                indexIterator = 0;
             }//swap them
-            
-             //if the right child is smaller than the parent  
-             if(this.list[indexIterator] > this.list[rightChild] && rightChild<=this.list.length-1){
+
+            //if the right child is smaller than the parent  
+            if (this.list[indexIterator] < this.list[rightChild] && rightChild <= this.list.length - 1) {
                 let temp = this.list[rightChild];
                 this.list[rightChild] = this.list[indexIterator];
                 this.list[indexIterator] = temp;
-   
-                indexIterator=0;
+
+                indexIterator = 0;
             }//swap them
 
         }
-            return this.list;
-        
-    }//builds the heap so that the smallest element is on top
+        return this.list;
+
+    }//builds the heap so that the largest element is on top 
+
+    heapify(index, list) {
+
+        let leftChild = index * 2 + 1;
+        let rightChild = index * 2 + 2;
+
+        //if the left child is bigger than the current element we are examining
+        if (this.list[leftChild] > this.list[index] && leftChild <= this.list.length - 1) {
+
+            let temp = this.list[leftChild];
+            this.list[leftChild] = this.list[index];
+            this.list[index] = temp;
+            //swap them
+
+            this.heapify(leftChild, list);
+            //call heapify again but with the index for the left child since we swapped them
+
+        }//swap them than call heapify again  
+
+        //if the right child is bigger than the current element we are examining
+        if (this.list[rightChild] > this.list[index] && rightChild <= this.list.length - 1) {
+
+            let temp = this.list[rightChild];
+            this.list[rightChild] = this.list[index];
+            this.list[index] = temp;
+            //swap them call heapify again
+
+            this.heapify(rightChild, list);
+            //call heapify again but with the index for the right child since we swapped them
+        }
+
+        else {
+            return list;
+        }//base case neither the left nor the right child are bigger than the current value
+         //
+
+    }//assume that we only have the top element out of place in the heap
+    //goes through based on that and sorts it until it is in a correct spot
 
     //the sorting instance of the respective class
     sort() {
-       
-        let arrayLength = this.list.length;//assign our array length to be equal to the list length
+        this.list = this.buildMaxHeap();
+        //build the heap
+        
+        let currentArray=[];
+        //make a default array
 
-        for(let i = Math.floor(arrayLength/2);i>=0; i-=1){
-            this.heapify(this.list,i);
+        
+        while(this.list.length > 0){
+
+            currentArray.unshift(this.list[0]);
+            //unshift function adds the max element to our currentArray 
+
+            this.list.shift();
+            //shift function removes the first element in the array,which in a heap is our max
+
+            this.heapify(0,this.list);
+            //recrusively scan to see if our new max is the correct one
         }
+        
+        
+        //declare our max as the top of the array
+        //swap the top of the heap with the bottom
 
-        for(let i = this.list.length-1; i >0; i--){
-            this.swap(this.list,0,i);
-            arrayLength--;
-
-            this.heapify(this.list,0);
-        }
-       return this.list;
+        //place the top of the array at the end of a new array
+        //rebuild the heap
+        //assign the currentArray we have built as the new list
+        this.list = currentArray; 
+        return this.list;
     }//sort
 
-    
-    getIndexIterator(){
-        return this.indexIterator;
-    }//returns the index Iterator
+
+
 
 }//class heap: Minor issue with it source from where i based my code https://www.w3resource.com/javascript-exercises/searching-and-sorting-algorithm/searching-and-sorting-algorithm-exercise-3.php
-   
-class heapTree{
-    constructor(list) {
-        this.root = null;
-        //the parent node of everything
-        this.list=list;
-        //the array we pass to it
-        this.maxElements = list.length;
-        //the maximum elements that should be present within the array
-        
-    }
 
-
-    buildMinHeap(list){
-
-        //iterator through the entire array and 
-        for(let indexIterator = 0; indexIterator <= list.length - 1; indexIterator++){
-            
-
-            //if the left child is smaller than the parent    
-            if(list[indexIterator]>list[indexIterator*2+1]){
-                let temp = list[indexIterator];
-                list[indexIterator] = list[indexIterator*2+1];
-                list[indexIterator*2+1] = temp;
-            }//swap them
-            
-             //if the right child is smaller than the parent  
-            if(list[indexIterator]>list[indexIterator*2+2]){
-                let temp = list[indexIterator];
-                list[indexIterator] = list[indexIterator*2+2];
-                list[indexIterator*2+2] = temp;
-            }
-
-        }
-            return list;
-    }//builds the heap in an unorganized way
-
-
-}
-   
 
 
 class Bubble {
@@ -326,49 +335,49 @@ class Bubble {
 
     //the sorting instance of the respective class
     sort() {
-                        this.linkedList.add('start',this.list, 1);
-                        //save the event, the data for that event, and the line of javascript code
+        this.linkedList.add('start', this.list, 1);
+        //save the event, the data for that event, and the line of javascript code
 
-        if(this.list.length <= 1){
+        if (this.list.length <= 1) {
             return this.list;
         }//see if the list length is less than or equal to one if so just return it
-                        this.linkedList.add('check',this.list.length, 3);
-                        //save the event, the data for that event, and the line of javascript code
-        
-        let arrayLength=this.list.length;
+        this.linkedList.add('check', this.list.length, 3);
+        //save the event, the data for that event, and the line of javascript code
+
+        let arrayLength = this.list.length;
         //set the arrayLength equal to the length of the elements in the list
-                        this.linkedList.add('maxtorepeat',this.list.length, 5);
-                        //save the event, the data for that event, and the line of javascript code
-        for(arrayLength;arrayLength>0;arrayLength--){
+        this.linkedList.add('maxtorepeat', this.list.length, 5);
+        //save the event, the data for that event, and the line of javascript code
+        for (arrayLength; arrayLength > 0; arrayLength--) {
 
-            for(let index=0; index <= this.list.length-1; index++){
+            for (let index = 0; index <= this.list.length - 1; index++) {
 
-                this.linkedList.add('comparator',index, 5);
+                this.linkedList.add('comparator', index, 5);
                 //save the event, the data for that event, and the line of javascript code
-                this.linkedList.add('comparee',index, 5);
+                this.linkedList.add('comparee', index, 5);
                 //save the event, the data for that event, and the line of javascript code
 
-                if(this.list[index] > this.list[index+1]){    
+                if (this.list[index] > this.list[index + 1]) {
                     let temp = this.list[index];
-                    this.list[index]= this.list[index+1];
-                    this.list[index+1] = temp;
+                    this.list[index] = this.list[index + 1];
+                    this.list[index + 1] = temp;
 
-                    this.linkedList.add('swap',[index,index+1], 7);
+                    this.linkedList.add('swap', [index, index + 1], 7);
                     //save the event, the data for that event, and the line of javascript code
-                
+
                 }//if statment compare the current element to the next one
             }//inner for loop selects the neighbors
         }//outter for loop repeats the algorithim for n times
-        this.linkedList.add('end',this.list, 10);
-                      
+        this.linkedList.add('end', this.list, 10);
+
         return this.list;
     }
 
-    getLinkedList(){
+    getLinkedList() {
         return this.linkedList;
     }
 
-    getList(){
+    getList() {
         return this.list;
     }
 }//done but hasn't been unit tested or linked list hasnt been created
@@ -384,17 +393,17 @@ class Quick {
     //the sorting instance of the respective class
     sort() {
 
-        if(this.list.length <= 1){
+        if (this.list.length <= 1) {
             return this.list;
         }//check to see if the list is = to or smaller than 1
         //choose a pivot point here were using the median of three principle
     }
-    
-    getLinkedList(){
+
+    getLinkedList() {
         return this.linkedList;
     }
 
-    getList(){
+    getList() {
         return this.list;
     }
 
